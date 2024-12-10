ad77b5b35c15f8067867d8675a51efd2c5a66483
Anas-khan <anaskhanislamian728@gmail.com>
2024-12-11 01:08:18 +0500

    Day - 02 learning and mastering Python

---
+++ 100 Days Python/Day-2.py
@@ -0,0 +1,15 @@
+# Addition
+addition = 4 + 5
+print("Addition (4 + 5):", addition)
+
+# Subtraction
+subtraction = 5 - 5
+print("Subtraction (5 - 5):", subtraction)
+
+# Multiplication
+multiplication = 3 * 5
+print("Multiplication (3 * 5):", multiplication)
+
+# Division
+division = 10 / 2
+print("Division (10 / 2):", division)
