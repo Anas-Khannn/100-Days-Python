42e86fbda48f942ad3494132aa04000e3016e3e7
Anas-khan <anaskhanislamian728@gmail.com>
2024-12-12 02:11:27 +0500

    Day - 03 learning and mastering Python

---
+++ 100 Days Python/Day-3.py
@@ -0,0 +1,42 @@
+  
+# Learning Pyhton from Data Camp
+# Topics Covered in Day 3 Learning Python were
+   # 1- Variables and Data Types 
+   # Basic Simple Exerscises with it as well 
+
+
+# Create variables
+number_1 = 30
+number_2 = 40
+
+# Calculate result
+result = number_1 + number_2
+
+# Print out result
+print("Result == ", result)
+
+# ---------------------------------------------------------------#
+
+
+intro = "Hello! How are you?"
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+
+# ------------------------------------------------------------------# 
+
+# Create a variable half
+half = 0.5  
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True  
+
+
+# ----------------------------------------------------------#
+
